5510c51248259ca82c13681f95ee540b6599ea18
Fernando Donini Ramos fdr190@gmail.com <fernando.donini@kobe.io>
2019-04-18 17:34:47 -0300

    # kobe_components Import Class into components package

--- lib/kobe_components.dart
+++ lib/kobe_components.dart
@@ -2,3 +2,4 @@ library kobe_components;
 
 import 'package:kobe_components/widgets/button/kobe_button.dart';
 import 'package:kobe_components/extension/kobe_color.dart';
+import 'package:kobe_components/services/parsing/KobeColors.dart';
