e8d621afa513a5eb9f2a0f65cbec0a758616441d
Fernando Donini Ramos fdr190@gmail.com <fernando.donini@kobe.io>
2019-04-23 22:51:25 -0300

    # KobeComponents Library Attempt to initialize the KobeColor by using the json file in the bundle, but failed

--- lib/kobe_components.dart
+++ lib/kobe_components.dart
@@ -1,5 +1,39 @@
 library kobe_components;
 
+import 'dart:convert';
 import 'package:kobe_components/widgets/button/kobe_button.dart';
 import 'package:kobe_components/extension/kobe_color.dart';
+import 'dart:async' show Future;
 import 'package:kobe_components/services/parsing/KobeColors/kobe_colors.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+main() async {
+  //  TODO: Get file from Assets/Data/*.JSON
+  // String jsonStr = await loadDesignSettingsJsonString();
+  // parseKobeColor(jsonStr);
+}
+
+Future<String> loadDesignSettingsJsonString() async {
+  String jsonStr = await loadDesignSettingsJsonAsset();
+  return jsonStr;
+}
+
+Future loadDesignSettingsJsonAsset() async {  
+  return await rootBundle.load('assets/data/designSettings.json');
+}
+
+KobeColor parseKobeColor(String jsonStr) {  
+  var jsonData = jsonDecode(jsonStr);
+
+  return KobeColor(
+    jsonData['colors']['error'], 
+    jsonData['colors']['background'], 
+    jsonData['colors']['primaryVariant'], 
+    jsonData['colors']['foreground'], 
+    jsonData['colors']['primary'], 
+    jsonData['colors']['textColorHighEmphasis'], 
+    jsonData['colors']['textColorMediumEmphasis'], 
+    jsonData['colors']['listSeparator'], 
+    jsonData['colors']['line']
+  );  
+}
